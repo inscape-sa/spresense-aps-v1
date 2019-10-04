626629fa418417bd79fbc5bf0c4aa20b5c99eb40
Shingo Aoyagi <aoyagi@inscape.jp>
2019-10-04 11:13:01 +0900

    remove 00_sandbox(INVALID NAME), and add aps00_sandbox

--- _aps_samples/00_sandbox/maincore_test/maincore_test_main.c
+++
@@ -1,7 +0,0 @@
-#include <sdk/config.h>
-#include <stdio.h>
-
-int maincore_test_main(int argc, char *argv[])
-{
-  return 0;
-}
---
+++ _aps_samples/aps00_sandbox/aps_maincore/aps_maincore_main.c
@@ -0,0 +1,8 @@
+#include <sdk/config.h>
+#include <stdio.h>
+
+int aps_maincore_main(int argc, char *argv[])
+{
+  printf("Hello, World. by aps_maincore\n");
+  return 0;
+}
