193cf28c9a2354c7ffab6c70d49c8cb365d88071
Shingo Aoyagi <aoyagi@inscape.jp>
2019-11-05 11:03:01 +0900

    add Multicore-base-programs

---
+++ _aps_samples/aps00_sandbox/aps_multicore_worker/include/aps_multicore.h
@@ -0,0 +1,7 @@
+/* MP object keys. Must be synchronized with supervisor. */
+
+#define APS_MULTICOREKEY_SHM   1
+#define APS_MULTICOREKEY_MQ    2
+#define APS_MULTICOREKEY_MUTEX 3
+
+#define MSG_ID_APS_MULTICORE 1
