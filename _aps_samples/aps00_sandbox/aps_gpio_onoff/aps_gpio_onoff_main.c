cbaf60d3c9daf18c87da293afeccd0724781afb5
Shingo Aoyagi <aoyagi@inscape.jp>
2019-12-12 10:15:32 +0900

    compat Arduino : aps_gpio

--- _aps_samples/aps00_sandbox/aps_gpio_onoff/aps_gpio_onoff_main.c
+++ _aps_samples/aps00_sandbox/aps_gpio_onoff/aps_gpio_onoff_main.c
@@ -10,62 +10,82 @@
 
 /* EXEC times */
 #define LOOP_MAX      (20)
-#define LED_OFF       (0)
 
 /* BUTTON DEF */
-#define GPIO_BUTTON_L (29)
-#define GPIO_BUTTON_R (39)
-#define GPIO_LED_L    (46)
-#define GPIO_LED_R    (47)
+#define GPIO_BUTTON_L (39)
+#define GPIO_BUTTON_R (29)
+#define GPIO_LED_L    (47)
+#define GPIO_LED_R    (46)
 
+/* Arduino Compat Layer */
+#define INPUT         (true)
+#define OUTPUT        (false)
+#define HIGH          (1)
+#define LOW           (0)
+
+/* prototype definitions */
+static void pinMode(int pin, bool mode);
+
+/* private variables */
+int cnt;
+
+static void setup(void)
+{
+  /* setup GPIO */
+  pinMode(GPIO_BUTTON_L, INPUT);
+  pinMode(GPIO_BUTTON_R, INPUT);
+  pinMode(GPIO_LED_L, OUTPUT);
+  pinMode(GPIO_LED_R, OUTPUT);
+
+  /* Clear Counter */
+  cnt = 0;
+}
+
+static void loop(void)
+{
+  int inputLeft = LOW;
+  int inputRight = LOW;
+
+  board_gpio_write(GPIO_LED_L, LOW);
+  board_gpio_write(GPIO_LED_R, LOW);
+  sleep(1);
+  
+  inputRight = board_gpio_read(GPIO_BUTTON_R);
+  inputLeft = board_gpio_read(GPIO_BUTTON_L);
+  board_gpio_write(GPIO_LED_R, inputRight);
+  board_gpio_write(GPIO_LED_L, inputLeft);
+  sleep(1);
+
+  cnt++;
+  if (cnt >= LOOP_MAX) {
+    while(1);
+      /* NOP */
+  }
+}
+
+/**
+ * public -- MAIN
+ */
 int aps_gpio_onoff_main(int argc, char *argv[])
 {
-  int ret;
-  int pin;
-  int cnt;
-
-  /* GPIO input settings */
-  pin = GPIO_BUTTON_L;
-  ret = board_gpio_config(pin, 0, true, false, PIN_FLOAT);
-  if (ret != 0) {
-    printf("ERROR:board_gpio_config(%d)", pin);
-    return -1;
+  setup();
+  while (1) {
+    loop();
   }
-  pin = GPIO_BUTTON_R;
-  ret = board_gpio_config(pin, 0, true, false, PIN_FLOAT);
-  if (ret != 0) {
-    printf("ERROR:board_gpio_config(%d)", pin);
-    return -1;
-  }
-
-  /* GPIO output settings */
-  pin = GPIO_LED_R;
-  ret = board_gpio_config(pin, 0, false, false, PIN_FLOAT);
-  if (ret != 0) {
-    printf("ERROR:board_gpio_config(%d)", pin);
-    return -1;
-  }
-  pin = GPIO_LED_L;
-  ret = board_gpio_config(pin, 0, false, false, PIN_FLOAT);
-  if (ret != 0) {
-    printf("ERROR:board_gpio_config(%d)", pin);
-    return -1;
-  }
-
-  for (cnt = 0; cnt < LOOP_MAX; cnt++) {
-    int inputL = LED_OFF;
-    int inputR = LED_OFF;
-  
-    board_gpio_write(GPIO_LED_L, LED_OFF);
-    board_gpio_write(GPIO_LED_R, LED_OFF);
-    sleep(1);
-    
-    inputL = board_gpio_read(GPIO_BUTTON_L);
-    inputR = board_gpio_read(GPIO_BUTTON_R);
-    board_gpio_write(GPIO_LED_L, inputL);
-    board_gpio_write(GPIO_LED_R, inputR);
-    sleep(1);
-  }
+  /* NEVER REACHED --- */
   return 0;
+}
 
+/**
+ * private -- Arduino Compat
+ */
+static void pinMode(int pin, bool mode)
+{
+  int ret;
+  ret = board_gpio_config(pin, 0, mode, false, PIN_FLOAT);
+  if (ret != 0) {
+    printf("ERROR:board_gpio_config(%d)", pin);
+    while(1)
+      ; /* HALT */
+  }
 }
