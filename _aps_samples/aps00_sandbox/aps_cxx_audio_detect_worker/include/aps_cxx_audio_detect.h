0621b84212afe9be7b9b00b5e164f53fda19fea4
Shingo Aoyagi <aoyagi@inscape.jp>
2019-12-17 08:46:22 +0900

    add sub-process for aps_cxx_audio_detect (as aps_cxx_audio_detect_worker)

---
+++ _aps_samples/aps00_sandbox/aps_cxx_audio_detect_worker/include/aps_cxx_audio_detect.h
@@ -0,0 +1,7 @@
+/* MP object keys. Must be synchronized with supervisor. */
+
+#define APS_CXX_AUDIO_DETECTKEY_SHM   1
+#define APS_CXX_AUDIO_DETECTKEY_MQ    2
+#define APS_CXX_AUDIO_DETECTKEY_MUTEX 3
+
+#define MSG_ID_APS_CXX_AUDIO_DETECT 1
