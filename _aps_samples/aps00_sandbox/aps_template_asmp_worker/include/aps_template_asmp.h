3ecd5b54a2d38eab8cf8ee1c02090f675542b532
Shingo Aoyagi <aoyagi@inscape.jp>
2019-12-16 11:10:18 +0900

    add template ASMP Application

---
+++ _aps_samples/aps00_sandbox/aps_template_asmp_worker/include/aps_template_asmp.h
@@ -0,0 +1,7 @@
+/* MP object keys. Must be synchronized with supervisor. */
+
+#define APS_TEMPLATE_ASMPKEY_SHM   1
+#define APS_TEMPLATE_ASMPKEY_MQ    2
+#define APS_TEMPLATE_ASMPKEY_MUTEX 3
+
+#define MSG_ID_APS_TEMPLATE_ASMP 1
