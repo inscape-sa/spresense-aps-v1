b029613f994eaa130e1d72550073666705d51b8e
Shingo Aoyagi <aoyagi@inscape.jp>
2019-12-20 20:38:22 +0900

    fixing aps_camera_asmp, but not already fixed [migrate from aps00_sandbox to aps01_sandbox]

---
+++ _aps_samples/aps01_sandbox/aps_camera_v2_asmp/config_basic.h
@@ -0,0 +1,19 @@
+#ifndef __APPS_EXAMPLES_CONFIG_BASIC_H
+#define __APPS_EXAMPLES_CONFIG_BASIC_H
+
+#if CONFIG_NFILE_DESCRIPTORS < 1
+#  error "You must provide file descriptors via CONFIG_NFILE_DESCRIPTORS in your configuration file"
+#endif
+
+#define message(format, ...)    printf(format, ##__VA_ARGS__)
+#define err(format, ...)        fprintf(stderr, format, ##__VA_ARGS__)
+
+/**
+ * Configuration for ASMP 
+ */
+/* Include worker header */
+#include "aps_camera_v2_asmp.h"
+/* Worker ELF path */
+#define WORKER_FILE "/mnt/spif/aps_camera_v2_asmp"
+
+#endif /* __APPS_EXAMPLES_CONFIG_BASIC_H */
